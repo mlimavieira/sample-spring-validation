33c6218071b678ea734182895eee498e88f960e4
Marcio Vieira <mlimavieira@gmail.com>
2019-04-28 19:50:18 +0100

    no message

--- src/main/java/com/mlimavieira/samplespringvalidation/controller/dto/BeerDto.java
+++ src/main/java/com/mlimavieira/samplespringvalidation/controller/dto/BeerDto.java
@@ -1,4 +1,14 @@
 package com.mlimavieira.samplespringvalidation.controller.dto;
 
+import lombok.Data;
+
+@Data
 public class BeerDto {
+
+    private Long id;
+    private String name;
+    private String type;
+    private Double price;
+
+
 }
--- src/main/java/com/mlimavieira/samplespringvalidation/service/entity/Beer.java
+++ src/main/java/com/mlimavieira/samplespringvalidation/service/entity/Beer.java
@@ -1,4 +1,13 @@
 package com.mlimavieira.samplespringvalidation.service.entity;
 
+import lombok.Data;
+
+@Data
 public class Beer {
+
+    private Long id;
+    private String name;
+    private String type;
+    private Double price;
+
 }
